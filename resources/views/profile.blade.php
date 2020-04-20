2f7139cdd5f41f0cd91866e9f1d37a8ff08bbb31
rubinarana <rubinaranamgr@gmail.com>
2020-04-20 23:30:21 +0545

    last modified

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -52,7 +52,7 @@
                             <li class="dropdown-header"><a href="#" class="dropdown-item">View Profile</a></li>
                         </ul>
                     </li>
-                    
+
                     <li class="nav-item">
                         <a href="#" class="nav-link" title="Settings" data-toggle="tooltip" data-placement="top"><i class="fa fa-cogs fa-2x"></i></a>
                     </li>
@@ -62,8 +62,7 @@
     </nav>
 </div>
     <div class="container">
-        <div class="row mt-5 " style="overflow: hidden;">
-            <div class="row" id="aboutme">
+        <div class="row mt-5" id="aboutme" style="overflow: hidden;">
                 <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
                     <div class="image">
                         <img src="image/profile1.png" class="img-fluid">
@@ -71,42 +70,63 @@
                 </div>
 
                 <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
-                    <p>I am Rubina Rana. I am student and currently doing my bachelors degree in Bsc. CSIT(Computer Science and Internet Technology) from Asian College Of Higher Studies. Beside this, I love sports and do some sketches during my free time.</p>
-                    <hr style="border: 0px;height: 60px; width: 100%; background-image: url(image/type_7.png);background-repeat:repeat-x;">
-                    <h2>Personal Information</h2>
-                        <table class="table table-borderless">
-                            <tbody>
-                              <tr>
-                                <td><i class="fa fa-phone-square fa-2x"></i></td>
-                                <td>9860294884</td>
-                              </tr>
-                              <tr>
-                                <td><i class="fa fa-envelope fa-2x"></i></td>
-                                <td><a href="#" target="_blank">rubinaranamgr@gmail.com</a></td>
-                                </td>
-                              </tr>
-                              <tr>
-                                <td><i class="fa fa-map-marker fa-2x"></i></td>
-                                <td>Handigaun, Kathmandu</td>
-                                </td>
-                              </tr>
-                            </tbody>
-                        </table>
+                    <h1 class="text-center">Rubina Rana</h1><br>
+                         <div class="d-flex justify-content-center">
+                             <i class="fa fa-map-marker fa-2x"></i>&emsp;&emsp;
+                                   <h6> Handigaun, Kathmandu</h6>
+                        </div><br>
+
+                        <div class="d-flex justify-content-center">
+                             <i class="fa fa-phone-square fa-2x"></i>&emsp;
+                                <h6>9860294884 / 9803208339</h6>
+                        </div><br>
+                        
+                         <div class="d-flex justify-content-center">
+                             <i class="fa fa-envelope fa-2x"></i>&emsp;
+                                <a href="#" target="_blank">rubinaranamgr@gmail.com</a>
+                        </div>
+
+                        
+
+                        <hr style="border: 0px;height: 60px; width: 100%; background-image: url(image/type_7.png);background-repeat:repeat-x;">
+                        <div class="d-flex justify-content-around">
+                            <button type="button" class="btn btn-success">
+                                Friends <span class="badge badge-light">4</span>
+                            </button>
+                            <button type="button" class="btn btn-danger">
+                                Following <span class="badge badge-light">7</span>
+                            </button>
+                             <button type="button" class="btn btn-info">
+                                Posts <span class="badge badge-light">7</span>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         
     </div>
    <hr><br>
    <div>
-       <div class="footer">
-        <h2 style="text-align: center;">Connected With</h2>
-        <div class="media">
-            <a href="https://www.facebook.com/rubina.magar.71" target="_blank"><i class="fa fa-facebook fa-3x"></i></a>
-            <a href="https://github.com/rubinarana" target="_blank"><i class="fa fa-github fa-3x"></i></a>
-            <a href="https://www.linkedin.com/in/rubina-rana-396a7016a/" target="_blank"><i class="fa fa-linkedin fa-3x"></i></a>
+
+<div class="footer" style="overflow: hidden;">
+    <div class="row">
+        <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+            <div class="container">
+            <h2 class="text-center">About</h2><br>
+                <p>I am Rubina Rana. I am student and currently doing my bachelors degree in Bsc. CSIT(Computer Science and Internet Technology) from Asian College Of Higher Studies. Beside this, I love sports and do some sketches during my free time.</p> 
+            </div>
+        </div>
+
+        <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+             <h2 style="text-align: center;">Connected With</h2>
+            <div class="media">
+                    <a href="https://www.facebook.com/rubina.magar.71" target="_blank"><i class="fa fa-facebook fa-3x"></i></a>
+                    <a href="https://github.com/rubinarana" target="_blank"><i class="fa fa-github fa-3x"></i></a>
+                    <a href="https://www.linkedin.com/in/rubina-rana-396a7016a/" target="_blank"><i class="fa fa-linkedin fa-3x"></i></a>
+            </div>
         </div>
     </div>
-   </div>
+</div>
    <script type="text/javascript">
         $(document).ready(function(){
             $('[data-toggle="tooltip"]').tooltip();
